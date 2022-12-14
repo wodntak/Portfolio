984647f72e3ab5f6033382377d3346701d92b363
jyang12122 <yyasu1212@naver.com>
2022-12-14 18:50:26 +0900

    12/14 progress
    - Added hover animations for Sun, Mercury
    
    - Added tooltip for Sun, Mercury
    
    - Mercury's tooltip color has to be changed

--- src/website/Planets/Mercury.js
+++ src/website/Planets/Mercury.js
@@ -1,11 +1,25 @@
 import React from 'react';
+import OverlayTrigger from 'react-bootstrap/OverlayTrigger';
+import Tooltip from 'react-bootstrap/Tooltip';
 import mercurymenu from './MenuStyle/Mercury_menu.module.scss';
 import mercuryani from './AnimationStyle/Mercury_animation.module.scss';
 
 const Mercury = ({checked}) => {
+
+    const renderTooltip = (props) => (
+        <Tooltip id={mercurymenu.Tooltip} {...props}>
+          About Me
+          <p className={mercurymenu.p}> 제 정보가 적혀있어요!</p>
+        </Tooltip>
+    );
     return (
         <>
+        <OverlayTrigger
+        placement="top"
+        overlay={renderTooltip}
+        >
             <div id= {checked ? mercuryani.mercury_animode : mercurymenu.mercury_menumode}></div>
+        </OverlayTrigger>
         </>
     );
 };
--- src/website/Planets/Sun.js
+++ src/website/Planets/Sun.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import Offcanvas from 'react-bootstrap/Offcanvas';
+import OverlayTrigger from 'react-bootstrap/OverlayTrigger';
+import Tooltip from 'react-bootstrap/Tooltip';
 import sunmenuStyle from './MenuStyle/Sun_menu.module.scss';
 import sunaniStyle from './AnimationStyle/Sun_animation.module.scss';
 
@@ -12,23 +14,35 @@ const Sun = ({checked}, ...props) => {
     const handleClose = () => setShow(false);
     const toggleShow = () => setShow((s) => !s);
 
+    const renderTooltip = (props) => (
+        <Tooltip id={sunmenuStyle.Tooltip} {...props}>
+          Menu
+          <p className={sunmenuStyle.p}> 클릭해서 메뉴를 여세요!</p>
+        </Tooltip>
+    );
+
     return (
-        <>
+    <>
+        <OverlayTrigger
+        placement="right"
+        overlay={renderTooltip}
+        >
         {checked ? <div id={sunaniStyle.sun_animode}/>  : 
         <div onClick={toggleShow} className="me-2" id={sunmenuStyle.sun_menumode}>
         </div>}
-
+        </OverlayTrigger>
         <Offcanvas id={sunmenuStyle.Offcanvas} show={show} onHide={handleClose} {...props}>
         <Offcanvas.Header className={sunmenuStyle.Offcanvas_Header} closeButton closeVariant="white">
         </Offcanvas.Header>
         <Offcanvas.Title className={sunmenuStyle.Offcanvas_Title}>About Me</Offcanvas.Title>
-        <Offcanvas.Title className={sunmenuStyle.Offcanvas_Title}>Skills</Offcanvas.Title>
-        <Offcanvas.Title className={sunmenuStyle.Offcanvas_Title}>Projects</Offcanvas.Title>
-        <Offcanvas.Title className={sunmenuStyle.Offcanvas_Title}>Contact Me</Offcanvas.Title>
         <Offcanvas.Title className={sunmenuStyle.Offcanvas_Title}>Education</Offcanvas.Title>
+        <Offcanvas.Title className={sunmenuStyle.Offcanvas_Title}>Skills</Offcanvas.Title>
+        <Offcanvas.Title className={sunmenuStyle.Offcanvas_Title}>Project 1</Offcanvas.Title>
+        <Offcanvas.Title className={sunmenuStyle.Offcanvas_Title}>Project 2</Offcanvas.Title>
+        <Offcanvas.Title className={sunmenuStyle.Offcanvas_Title}>Project 3</Offcanvas.Title>
+        <Offcanvas.Title className={sunmenuStyle.Offcanvas_Title}>Contact Me</Offcanvas.Title>
         <Offcanvas.Title className={sunmenuStyle.Offcanvas_Title}>Others</Offcanvas.Title>
-        <Offcanvas.Title className={sunmenuStyle.Offcanvas_Title}>1</Offcanvas.Title>
-        <Offcanvas.Title className={sunmenuStyle.Offcanvas_Title}>2</Offcanvas.Title>
+       
         </Offcanvas>
     </>
     );
