201153b51382861ec84e703f0c9364a57f1ecf7d
jyang12122 <yyasu1212@naver.com>
2022-12-17 23:17:55 +0900

    12/17
    Modal + Carousel

--- src/website/Planets/Mercury.js
+++ src/website/Planets/Mercury.js
@@ -2,10 +2,16 @@ import React from 'react';
 import OverlayTrigger from 'react-bootstrap/OverlayTrigger';
 import Tooltip from 'react-bootstrap/Tooltip';
 import Carousel from 'react-bootstrap/Carousel';
+import Modal from 'react-bootstrap/Modal';
+import { useState } from 'react';
 import mercurymenu from './MenuStyle/Mercury_menu.module.scss';
 import mercuryani from './AnimationStyle/Mercury_animation.module.scss';
 
 const Mercury = ({checked}) => {
+    const [show, setShow] = useState(false);
+
+    const handleClose = () => setShow(false);
+    const handleShow = () => setShow(true);
 
     const renderTooltip = (props) => (
         <Tooltip id={mercurymenu.Tooltip} {...props}>
@@ -19,8 +25,62 @@ const Mercury = ({checked}) => {
         placement="top"
         overlay={renderTooltip}
         >
-            {checked ? <div id={mercuryani.mercury_animode}/> : <div id={mercurymenu.mercury_menumode} onClick={}/>}
+            {checked ? <div id={mercuryani.mercury_animode}/> : <div id={mercurymenu.mercury_menumode} onClick={handleShow}/>}
         </OverlayTrigger>
+
+        <Modal
+        show={show}
+        size="lg"
+        id={mercurymenu.Modal}
+        onHide={handleClose}
+        backdrop="static"
+        keyboard={false}
+      >
+        <Modal.Header closeButton>
+          <Modal.Title>Modal title</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+        <Carousel>
+      <Carousel.Item>
+        <img
+          className="d-block w-100"
+          src="holder.js/800x400?text=First slide&bg=373940"
+          alt="First slide"
+        />
+        <Carousel.Caption>
+          <h3>First slide label</h3>
+          <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item>
+        <img
+          className="d-block w-100"
+          src="/"
+          alt="Second slide"
+        />
+
+        <Carousel.Caption>
+          <h3>Second slide label</h3>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item>
+        <img
+          className="d-block w-100"
+          src="holder.js/800x400?text=Third slide&bg=20232a"
+          alt="Third slide"
+        />
+
+        <Carousel.Caption>
+          <h3>Third slide label</h3>
+          <p>
+            Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+          </p>
+        </Carousel.Caption>
+      </Carousel.Item>
+    </Carousel>
+        </Modal.Body>
+      </Modal>
         </>
     );
 };
