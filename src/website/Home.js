7aeada4029c196d75e4af2b7d27499a496db003a
jyang12122 <yyasu1212@naver.com>
2023-01-04 17:59:19 +0900

    모바일 모드
    -모바일 반응형/ 태블릿 반응형
    - 태양, 기본 백그라운드 조정 완료
    - 태양, 기본 스크립트 주석 추가
    - 깔끔한 인터페이스위해 클릭해보세요 문구 제거
    - 모바일 모드 우주선 및 메뉴 모양 추가 및 위치 조정

--- src/website/Home.js
+++ src/website/Home.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Form from 'react-bootstrap/Form';
 import background from './Home.module.scss';
@@ -21,8 +21,11 @@ import { useState } from 'react';
 
 
 const Home = () => {
-  const width = window.innerWidth;
-  const height = window.innerHeight;
+
+  const setScreen = () => {
+    const vh =  window.innerHeight * 0.01;
+    document.documentElement.style.setProperty("--vh", `${vh}px`);
+  }
 
 
     const [checked, setChecked] = useState(false);
@@ -57,14 +60,16 @@ const Home = () => {
     const uranusShow = () => setUranus(true);
     const neptuneShow = () => setNeptune(true);
 
-  
+    // useEffect(() => {
+    //   setScreen();
+    // },[])
 
     return (
     <>
-        <div id={background.star1}></div>
-        <div id={background.star2}></div>
-        <div id={background.star3}></div>
-        <div id={background.star4}></div>
+      <div id={background.star1}></div>
+      <div id={background.star2}></div>
+      <div id={background.star3}></div>
+      <div id={background.star4}></div>
         <Sun checked={checked} mercuryShow={mercuryShow} venusShow={venusShow} earthShow={earthShow} marsShow={marsShow} jupiterShow={jupiterShow} saturnShow={saturnShow} uranusShow={uranusShow} neptuneShow={neptuneShow}/>
         <Mercury checked={checked} mercuryshow={mercuryshow} mercuryClose={mercuryClose} mercuryShow={mercuryShow}/>
         {/* <Venus checked={checked} venusshow={venusshow} venusClose={venusClose} venusShow={venusShow}/>
@@ -75,29 +80,28 @@ const Home = () => {
         <Uranus checked={checked} uranusshow={uranusshow} uranusClose={uranusClose} uranusShow={uranusShow}/>
         <Neptune checked={checked} neptuneshow={neptuneshow} neptuneClose={neptuneClose} neptuneShow={neptuneShow}/> */}
         
-    <Form>
-      <Form.Check 
+      <Form>
+        <Form.Check 
         type="switch"
         id="custom-switch"
         className={background.switch}
         checked={checked}
-        label={checked ? "애니메이션 모드 (미완성)" + width : "메뉴 모드" + height}
+        label={checked ? "애니메이션 모드 (미완성)" + window.innerWidth : "메뉴 모드" + window.innerHeight}
         onChange={(e) => setChecked(e.currentTarget.checked)} 
       />
-      <Container>
-        <Row>
-          <Col id={!checked ? background.mobilemode : background.desktopmode}>
-            <img className={background.introicon}  src="https://img.icons8.com/external-bearicons-outline-color-bearicons/100/null/external-Space-location-bearicons-outline-color-bearicons.png" alt="spaceship"/>
-            <h5 className={background.h5_intro}> Menu </h5>
-          </Col>
-          <Col>
-            <svg className={background.menuicon}  clip-rule="evenodd" fill-rule="evenodd" stroke-linejoin="round" stroke-miterlimit="2" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path d="m11.998 21.995c5.517 0 9.997-4.48 9.997-9.997 0-5.518-4.48-9.998-9.997-9.998-5.518 0-9.998 4.48-9.998 9.998 0 5.517 4.48 9.997 9.998 9.997zm4.843-8.211c.108.141.157.3.157.456 0 .389-.306.755-.749.755h-8.501c-.445 0-.75-.367-.75-.755 0-.157.05-.316.159-.457 1.203-1.554 3.252-4.199 4.258-5.498.142-.184.36-.29.592-.29.23 0 .449.107.591.291z" fill-rule="nonzero"/></svg>
-            <h5 className={ background.h5_menu} > 여기를 클릭해보세요! </h5>
-          </Col>
-        </Row>
-      </Container>
-      
-    </Form>
+        <Container>
+          <Row>
+            <Col id={!checked ? background.mobilemode : background.desktopmode}>
+              <img className={background.introicon} src="https://img.icons8.com/external-bearicons-outline-color-bearicons/100/null/external-Space-location-bearicons-outline-color-bearicons.png" alt="spaceship"/>
+              <h5 className={background.h5_intro}> Menu </h5>
+            </Col>
+            <Col>
+              {/* <svg className={background.menuicon} clip-rule="evenodd" fill-rule="evenodd" stroke-linejoin="round" stroke-miterlimit="2" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path d="m11.998 21.995c5.517 0 9.997-4.48 9.997-9.997 0-5.518-4.48-9.998-9.997-9.998-5.518 0-9.998 4.48-9.998 9.998 0 5.517 4.48 9.997 9.998 9.997zm4.843-8.211c.108.141.157.3.157.456 0 .389-.306.755-.749.755h-8.501c-.445 0-.75-.367-.75-.755 0-.157.05-.316.159-.457 1.203-1.554 3.252-4.199 4.258-5.498.142-.184.36-.29.592-.29.23 0 .449.107.591.291z" fill-rule="nonzero"/></svg>
+              <h5 className={ background.h5_menu} > 여기를 클릭해보세요! </h5> */}
+            </Col>
+          </Row>
+        </Container>
+      </Form>
     </>
 
     );
